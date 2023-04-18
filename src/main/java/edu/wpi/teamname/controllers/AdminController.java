7b4331d406ec6162bc2548102967d0eac62b7304
Prahladh Raja <pnamboorkrishnam@wpi.edu>
2023-04-18 00:36:46 -0400

    Tab pane with tables span the whole page + added columns to tables

--- src/main/java/edu/wpi/teamname/controllers/AdminController.java
+++ src/main/java/edu/wpi/teamname/controllers/AdminController.java
@@ -6,7 +6,12 @@ import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import java.util.ArrayList;
 import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -21,20 +26,16 @@ public class AdminController {
   @FXML MFXButton mealRequestsButton;
   @FXML MFXButton roomRequestsButton;
   @FXML MFXButton flowerRequestsButton;
-  ImageView floorView;
-  @FXML GesturePane mapView;
-  StackPane stackpane;
-  AnchorPane anchorpane = new AnchorPane();
 
-  Image floorL1 = new Image(String.valueOf(Main.class.getResource("images/00_thelowerlevel1.png")));
+  @FXML TableView<MealRequest> toDoTable;
+  public ObservableList<MealRequest> data = FXCollections.observableArrayList();
+  @FXML
+  TableColumn<MealRequest, String> deliveryID1 = new TableColumn<>("Service Request Type");
+  @FXML TableColumn<MealRequest, String> itemsOrdered1 = new TableColumn<>("Items Ordered");
+  @FXML TableColumn<MealRequest, String> timeOrdered1 = new TableColumn<>("Time Ordered");
+  @FXML TableColumn<MealRequest, String> orderer1 = new TableColumn<>("Orderer");
+  @FXML TableColumn<MealRequest, String> status1 = new TableColumn<>("Status");
 
-  Image floorL2 = new Image(String.valueOf(Main.class.getResource("images/00_thelowerlevel2.png")));
-
-  Image floor1 = new Image(String.valueOf(Main.class.getResource("images/01_thefirstfloor.png")));
-
-  Image floor2 = new Image(String.valueOf(Main.class.getResource("images/02_thesecondfloor.png")));
-
-  Image floor3 = new Image(String.valueOf(Main.class.getResource("images/03_thethirdfloor.png")));
 
   @FXML
   public void initialize() {
@@ -43,124 +44,8 @@ public class AdminController {
     mealRequestsButton.setOnMouseClicked(event -> goToMealRequestsSubmittedPage());
     roomRequestsButton.setOnMouseClicked(event -> goToRoomRequestsSubmittedPage());
     flowerRequestsButton.setOnMouseClicked(event -> goToFlowerRequestsSubmittedPage());
-
-    //
-    //    stackpane = new StackPane();
-    //    floorView =
-    //        new ImageView(
-    //            new
-    // Image(String.valueOf(Main.class.getResource("images/00_thelowerlevel2.png"))));
-    //    floorL2Button.setStyle("-fx-background-color: #1D2B94");
-    //    stackpane.setPrefSize(800, 522);
-    //    mapView.setContent(stackpane);
-    //    // stackpane.setBackground(Background.fill(Color.RED));
-    //
-    //    floorView.setImage(floor1);
-    //    stackpane.getChildren().add(floorView);
-    //    stackpane.getChildren().add(anchorpane);
-    //    anchorpane.setBackground(Background.fill(Color.TRANSPARENT));
-    //
-    //    floorL1Button.setOnMouseClicked(event -> switchToFloorL1());
-    //    floorL2Button.setOnMouseClicked(event -> switchToFloorL2());
-    //    floor1Button.setOnMouseClicked(event -> switchToFloor1());
-    //    floor2Button.setOnMouseClicked(event -> switchToFloor2());
-    //    floor3Button.setOnMouseClicked(event -> switchToFloor3());
-
-    //    mapView.setMinScale(0.005);
-    //    mapView.setScrollBarPolicy(GesturePane.ScrollBarPolicy.NEVER);
-    //    Platform.runLater({
-    //          mapView.zoomTo(0.01, new Point2D(2500, 1750))
-    //        }
-    //        );
   }
 
-  //  public void changeButtonColor() {
-  //    if (floorView.getImage().equals(floorL1)) {
-  //      floorL1Button.setStyle("-fx-background-color: #1D2B94");
-  //      floorL2Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor1Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor2Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor3Button.setStyle("-fx-background-color: #CAD6F8");
-  //
-  //    } else if (floorView.getImage().equals(floorL2)) {
-  //      floorL1Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floorL2Button.setStyle("-fx-background-color: #1D2B94");
-  //      floor1Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor2Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor3Button.setStyle("-fx-background-color: #CAD6F8");
-  //
-  //    } else if (floorView.getImage().equals(floor1)) {
-  //      floorL1Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floorL2Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor1Button.setStyle("-fx-background-color: #1D2B94");
-  //      floor2Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor3Button.setStyle("-fx-background-color: #CAD6F8");
-  //    } else if (floorView.getImage().equals(floor2)) {
-  //      floorL1Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floorL2Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor1Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor2Button.setStyle("-fx-background-color: #1D2B94");
-  //      floor3Button.setStyle("-fx-background-color: #CAD6F8");
-  //    } else if (floorView.getImage().equals(floor3)) {
-  //      floorL1Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floorL2Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor1Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor2Button.setStyle("-fx-background-color: #CAD6F8");
-  //      floor3Button.setStyle("-fx-background-color: #1D2B94");
-  //    }
-  //  }
-
-  //  public void switchToFloorL1() {
-  //    floorView.setImage(floorL1);
-  //    changeButtonColor();
-  //    // mapView.setContent(stackpane);
-  //  }
-  //
-  //  public void switchToFloorL2() {
-  //    floorView.setImage(floorL2);
-  //    changeButtonColor();
-  //    //    stackpane.getChildren().remove(floorView);
-  //    //    stackpane.getChildren().add(floorView);
-  //    // mapView.setContent(stackpane);
-  //  }
-  //
-  //  public void switchToFloor1() {
-  //    floorView.setImage(floor1);
-  //    changeButtonColor();
-  //    //    stackpane.getChildren().remove(floorView);
-  //    //    stackpane.getChildren().add(floorView);
-  //    //    mapView.setContent(stackpane);
-  //  }
-  //
-  //  public void switchToFloor2() {
-  //    floorView.setImage(floor2);
-  //    changeButtonColor();
-  //    //    stackpane.getChildren().remove(floorView);
-  //    //    stackpane.getChildren().add(floorView);
-  //    //    mapView.setContent(stackpane);
-  //  }
-  //
-  //  public void switchToFloor3() {
-  //    floorView.setImage(floor3);
-  //    changeButtonColor();
-  //    //    stackpane.getChildren().remove(floorView);
-  //    //    stackpane.getChildren().add(floorView);
-  //    //    mapView.setContent(stackpane);
-  //  }
-
-  List<Circle> floorL1Points = new ArrayList<>();
-  List<Circle> floorL2Points = new ArrayList<>();
-  List<Circle> floor1Points = new ArrayList<>();
-  List<Circle> floor2Points = new ArrayList<>();
-  List<Circle> floor3Points = new ArrayList<>();
-
-  public void showFloorL1locations() {
-    floorL1Points = new ArrayList<>();
-    floorL2Points = new ArrayList<>();
-    floor1Points = new ArrayList<>();
-    floor2Points = new ArrayList<>();
-    floor3Points = new ArrayList<>();
-  }
 
   public void goToMapEditorPage() {
     Navigation.navigate(Screen.BETTER_MAP_EDITOR);
